a3c3415820aa3f5d7213cb35d5036fcee0850b68
Amalia Zebi <amalia.zebi@b4finance.com>
2021-04-02 01:22:05 +0200

    edited my trip model for required fields

--- models/tripModel.js
+++ models/tripModel.js
@@ -17,6 +17,7 @@ const tripSchema = new mongoose.Schema(
       type: {
         type: String,
         enum: ["Point"],
+        required: true,
       },
       coordinates: {
         type: [Number],
